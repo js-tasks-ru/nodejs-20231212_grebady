5ee2674e58c25ee0533aeab504bfb1076d9fd564
Grefidy <grebennik2000@yandex.ru>
2024-09-03 12:37:12 +0300

    feat(2.1 задача решена)

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,21 @@
-const stream = require('stream');
+const stream = require('node:stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.currentLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    debugger;
+    if ((this.currentLength += chunk.length > this.limit)) {
+      callback(new LimitExceededError(), null);
+      return;
+    }
+    this.currentLength += chunk.length;
+    callback(null, chunk);
   }
 }
 
